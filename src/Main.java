26c70bad713098852d71e76f00f9b6c000ece261
mustafaaonatt <monat9494@gmail.com>
2023-02-08 21:12:23 -0500

    Initial commit : I created Sdet2, Project and Hello Word Class.

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+        System.out.print("Welcome Clarusway to SDET :  ");
+        System.out.println("This line is starting with the previous line");
+        System.out.println("Again we are starting with a new line");
+  } 
+}
