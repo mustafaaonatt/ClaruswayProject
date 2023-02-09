3736934f92b24ed93777723c97d35a7db9b8c00e
mustafaaonatt <monat9494@gmail.com>
2023-02-08 22:41:14 -0500

    I added Data types class.

---
+++ src/DateTypes.java
@@ -0,0 +1,24 @@
+public class DateTypes {
+    public static void main(String[] args) {
+        // Variables Declaration as below :
+        int age;
+        char initial;
+        // Assigning the variables as below:
+
+        age = 25;
+        initial = 'M';
+
+        int age1, height, distance;
+         age1 = 15;
+         height = 150;
+         distance = 75;
+        int age2 = 11, height1 = 120, distance2 = 100;
+
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(distance);
+
+        System.out.println(initial);
+
+    }
+}
