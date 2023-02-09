a01efdefbd8e85332e986ec413f3b447ec0544c9
mustafaaonatt <monat9494@gmail.com>
2023-02-08 23:10:50 -0500

    I added PrimitiveDataTypeDataypes class.

---
+++ src/PrimitiveDataType.java
@@ -0,0 +1,19 @@
+public class PrimitiveDataType {
+    public static void main(String[] args) {
+        boolean isClod = true;
+        boolean isHeavy = false;
+
+        char initial = 'C';
+
+        byte age = 22;
+
+        short salary = 1000;
+
+        System.out.println(isClod);
+        System.out.println(isHeavy);
+        System.out.println(initial);
+        System.out.println(age);
+        System.out.println(salary);
+
+    }
+}
