c0070f60a929caaa782a0206cbb23e4e3ff4dd4b
Mustafa <onat.mustafa@hotmail.ccom>
2023-03-18 18:37:55 -0400

    I created Bikes class from Vehicle class the inheritance example

---
+++ src/week6/HomeWork6/Vehicle/Bikes.java
@@ -0,0 +1,8 @@
+package week6.HomeWork6.Vehicle;
+
+public class Bikes extends Vehicle{
+    private  String tires;
+    public void seatHeight(){
+        System.out.println("You increased your seat height. ");
+    }
+}
