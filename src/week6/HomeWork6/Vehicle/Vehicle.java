fca513755c9db95ce0798cae915ae57e1add6afb
Mustafa <onat.mustafa@hotmail.ccom>
2023-03-18 18:16:59 -0400

    I created Vehicle class for the inheritance example

---
+++ src/week6/HomeWork6/Vehicle/Vehicle.java
@@ -0,0 +1,38 @@
+package week6.HomeWork6.Vehicle;
+
+public class Vehicle {
+    private String color;
+    private String type;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    private int size;
+    public void brake(){
+
+    }
+    public void showDetails(){
+
+    }
+}
