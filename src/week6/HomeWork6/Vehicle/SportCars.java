b75c492cbdc17dce3e2f7ad435e8b5773f9c7deb
Mustafa <onat.mustafa@hotmail.ccom>
2023-03-18 18:33:50 -0400

    I created SportCars class from Cars class the inheritance example

---
+++ src/week6/HomeWork6/Vehicle/SportCars.java
@@ -0,0 +1,10 @@
+package week6.HomeWork6.Vehicle;
+
+public class SportCars extends Cars{
+    private String model;
+
+    public void startNitro(){
+        System.out.println("You started nitro. ");
+    }
+
+}
