4857943188c7e53b4e6990975b1f08841cc7e450
Mustafa <monat9494@gmail.com>
2023-03-20 23:04:06 -0400

    I added some classes

---
+++ src/week7/Car.java
@@ -0,0 +1,15 @@
+package week7;
+
+public class Car {
+    protected int num1 = 10;
+    public String name = "Mustafa";
+    private   int num2 = 11;
+
+    public void setNum2(int num2){
+        this.num2 = num2;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+}
--- src/week7/Mammal.java
+++ src/week7/Mammal.java
@@ -10,10 +10,15 @@ public class Mammal extends Animal{
         this(15);
         System.out.println("Child Constructor");
     }
+    public Mammal(String name){
+        this();
+        System.out.println("name parameter");
+    }
 
     public static void main(String[] args) {
         Mammal mammal = new Mammal();
         Mammal mammal1 = new Mammal(25);
+        Mammal mammal2 = new Mammal("John");
     }
 }
 
