539a121a716e4df3028375c7f297cea7657d5bfe
Mustafa <monat9494@gmail.com>
2023-03-21 19:17:49 -0400

    I added a class

---
+++ src/week7/Signature.java
@@ -0,0 +1,24 @@
+package week7;
+
+public class Signature {
+    public static void main(String[] args) {
+
+    }
+    public int add(String a, int b){
+        return 0;
+
+    }
+
+    public int add(int b){
+        return 1;
+
+    }
+    public  int add(int b,String a){
+        return 2;
+
+    }
+
+    public int add2(String c, int e){
+        return 3;
+    }
+}
