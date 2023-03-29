f001d4a3dfced4e80196eea2434f284ce05681ca
Mustafa <monat9494@gmail.com>
2023-03-28 22:53:37 -0400

    I added some methods

--- src/week8/GameController/GameController.java
+++ src/week8/GameController/GameController.java
@@ -4,22 +4,16 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GameController {
-    Scanner scan = new Scanner(System.in);
-
-   static ArrayList <Game> games = new ArrayList<>();
-    public static void main(String[] args) {
-
-       games.add(new Sonic());
-       games.add(new Mario());
-       games.add(new Contra());
-
-       games.get(0).up();
-
-
-        char input = 'z';
-
-
-        /*
+    static Scanner scan = new Scanner(System.in);
+    static ArrayList<Game> games = new ArrayList<>();
+    static boolean isMainMenu = true;
+    static int mainMenuOption = 1;
+    static boolean isGameSelect = false;
+    static int gameMenuOption = 1;
+    static boolean isGamePlay = false;
+    static int selectedGame = 0;
+    static char input = 'z';
+     /*
         Up        -> U
         Down      -> D
         Right     -> R
@@ -27,20 +21,108 @@ public class GameController {
         A         -> A
         B         -> B
         Selection -> S
-         */
+     */
+
+    public static void main(String[] args) {
+
+        games.add(new Sonic());
+        games.add(new Mario());
+        games.add(new Contra());
+
 
         do {
-            input = mainMenu();
-        }while (input != 'p');
+            if (isMainMenu) {
+                input = mainMenu();
+                if (input == 'D') {
+                    if (mainMenuOption == 1) {
+                        mainMenuOption = 2;
+                    }
+                } else if (input == 'U') {
+                    mainMenuOption = 1;
 
+                } else if (input == 'S') {
+                    if (mainMenuOption == 1) {
+                        isGameSelect = true;
+                        isMainMenu = false;
+                        mainMenuOption = 1;
+                    } else {
+                        isGamePlay = true;
+                        isMainMenu = false;
+                        mainMenuOption = 1;
+                    }
+
+                }
+
+            }
+
+            if (isGameSelect) {
+                input = gameMenu();
+                if (input == 'D') {
+                    if (gameMenuOption == 3) {
+                        gameMenuOption++;
+                    }
+                } else if (input == 'U') {
+                    if (gameMenuOption != 1) {
+                        gameMenuOption--;
+
+                    } else if (input == 'S') {
+                        isMainMenu = true;
+                        isGameSelect = false;
+                        selectedGame = gameMenuOption - 1;
+                        gameMenuOption = 1;
+
+                    }
+                }
+                if (isGamePlay) {
+                    // TODO
+                }
+                System.out.println("\n\n");
+            }
+        }
+        while (input != 'P') ;
     }
+
+
+
     public static char mainMenu(){
         System.out.println("--------MAIN MENU--------");
-        System.out.println("-SELECTED GAME :  ");
-        System.out.println("Game Select");
-        System.out.println("Game Start");
-        System.out.println("-------------------------");
-        return  'p';
+        System.out.println("-SELECTED GAME : "+ (selectedGame == 0 ? "SONIC" : (selectedGame == 1 ? "MARIO" : "CONTRA")) + " -");
+        if (mainMenuOption == 1){
+            System.out.println("\t -> Game Select <- ");
+            System.out.println("Game Start");
+        } else if (mainMenuOption == 2) {
+            System.out.println("Game Select");
+            System.out.println("\t -> Game Start <-");
+        }
+
+        System.out.print("---(D)own---(U)p---(S)elect----");
+
+        return scan.next().charAt(0);
+
+    }
+
+
+    public static char gameMenu(){
+        System.out.println("--------MAIN MENU--------");
+        System.out.println("-SELECTED GAME : "+ (selectedGame == 0 ? "SONIC" : (selectedGame == 1 ? "MARIO" : "CONTRA")) + " -");
+        if (mainMenuOption == 1){
+            System.out.println("\t -> SONIC <- ");
+            System.out.println("MARIO");
+            System.out.println("CONTRA");
+        } else if (mainMenuOption == 2) {
+            System.out.println("SONIC");
+            System.out.println("\t -> MARIO <-");
+            System.out.println("CONTRA");
+        } else if (gameMenuOption == 3) {
+            System.out.println("SONIC");
+            System.out.println("MARIO");
+            System.out.println("\t -> CONTRA <-");
+
+        }
+
+        System.out.print("---(D)own---(U)p---(S)elect----");
+
+        return scan.next().charAt(0);
 
     }
 }
