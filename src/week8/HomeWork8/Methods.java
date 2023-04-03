ffc08f5f689b149c1ef2a17ace36ed9630336bce
Mustafa <monat9494@gmail.com>
2023-04-03 12:57:10 -0400

    made some changes

--- src/week8/HomeWork8/Methods.java
+++ src/week8/HomeWork8/Methods.java
@@ -1,5 +1,7 @@
 package week8.HomeWork8;
 
+import java.util.AbstractList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Methods {
@@ -8,19 +10,34 @@ public class Methods {
         Methods obj = new Methods();
         obj.createEmp();
     }
-    public  void createEmp(){//String fName, int kids, String education
-        System.out.print("Please enter your full name ");
-        String fname = input.nextLine();
-        System.out.print("How many kids dou you have ? ");
-        int kids = input.nextInt();
-        System.out.print("Please enter your last degree ? ");
-        String education = input.next();
+    public  void createEmp() {//String fName, int kids, String education
+
+        String fname = null;
+        int kids = 0;
+        String education = null;
+        int x =1;
+        do {
+            try {
+                System.out.print("Please enter your full name ");
+                fname = input.nextLine();
+                System.out.print("How many kids dou you have ? ");
+                kids = input.nextInt();
+                System.out.print("Please enter your last degree ? ");
+                education = input.next();
+                x=2;
+            } catch (Exception e) {
+                System.out.println("Please check your information and try again ");
+            }
+        }while (x==1);
 
 
-        EmployeePojo Emp01 = new EmployeePojo(fname,kids,education);
+        EmployeePojo Emp01 = new EmployeePojo(fname, kids, education);
         Emp01.setEmpID(1000);
-        Employee.employeelist.add(Emp01);
-        System.out.println(Employee.employeelist);
+        Database.employeelist.add(Emp01);
+        for (EmployeePojo e : Database.employeelist) {
+            System.out.print(e);
+        }
+
     }
 
 }
