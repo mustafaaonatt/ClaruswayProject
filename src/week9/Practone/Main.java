a3161da6c5d26585b62473ffbc2a1a7106dce1bd
Mustafa <monat9494@gmail.com>
2023-04-04 23:07:15 -0400

    I created  LinkList class

---
+++ src/week9/Practone/Main.java
@@ -0,0 +1,48 @@
+package week9.Practone;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+
+        list.add("Banana");
+        list.add("Bread");
+        list.add("Milk");
+        list.add("Egg");
+        list.add("Butter");
+
+        list.addFirst("Ice Cream");
+        list.addLast("Rice");
+        /*
+        list.removeLast();
+        list.removeFirst();
+        list.removeLast();
+
+        System.out.println(list);
+        */
+
+       String removed = list.remove();
+
+       list.add(3,"Corn");
+       list.set(3,"Cornflakes");
+
+       list.remove(5);
+
+        System.out.println(removed + " just removed recently. \n");
+
+        System.out.println("---------------------------------------");
+        Iterator<String> listIt = list.listIterator();
+
+        int counter = 1;
+        while (listIt.hasNext()){
+            System.out.println((counter++) + ". " + listIt.next());
+        }
+        System.out.println("----------------------------------");
+        counter = 1;
+        for (String str : list){
+            System.out.println((counter++) + ". " + str);
+        }
+    }
+}
