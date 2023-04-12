0a2a8eed96dcddbf947f4a8ce023c56a67588345
Mustafa <monat9494@gmail.com>
2023-04-11 22:02:38 -0400

    I added some more codes

--- src/week10/Maps/HasMapPract.java
+++ src/week10/Maps/HasMapPract.java
@@ -1,10 +1,12 @@
 package week10.Maps;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 public class HasMapPract {
     public static void main(String[] args) {
-        HashMap<String, Integer> languages = new HashMap<>();
+        LinkedHashMap<String, Integer> languages = new LinkedHashMap<>();
+        // HashMap<String, Integer> languages = new HashMap<>();
         // Capacity is the max number o times a hashMap can hold
         // Load factor is considered load percentage to trigger increase of Capacity
 
@@ -17,6 +19,17 @@ public class HasMapPract {
 
         System.out.println(languages.get("Java"));
 
+        languages.put("Java", 2008);
+        // Keys can not be duplicated, it will cause over writing of value
+
+        languages.putIfAbsent("PHP",2011);
+        // This approach can be used if you want to add something without disturbing what is already present
+
+        System.out.println(languages.get("Java"));
+
+        System.out.println(languages);
+
+        languages.putIfAbsent("PHP", 2013);
         System.out.println(languages);
     }
 }
