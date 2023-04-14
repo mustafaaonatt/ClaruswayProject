af65279af92d63ea3c39306009b02b9972104931
Mustafa <monat9494@gmail.com>
2023-04-13 23:10:45 -0400

    I made a MainClass  class for practice Enum

--- src/week10/MainClass.java
+++ src/week10/MainClass.java
@@ -1,12 +1,29 @@
 package week10;
 
+import java.util.Arrays;
+
 public class MainClass {
     public static void main(String[] args) {
+        // .compareTo() compares the ENUM constants based on their ordinal values/index number
         System.out.println(Size.EXTRALARGE.compareTo(Size.SMALL));
 
         System.out.println(Colors.RED.compareTo(Colors.GREEN));
+
+
         String enumValue = Size.SMALL.toString();
         System.out.println(enumValue);
+
+        String eValue = String.valueOf(Size.SMALL);
+        System.out.println(enumValue);
+
+        Size size = Size.LARGE;
+        System.out.println(size.name());
+
+        System.out.println(Size.valueOf("EXTRALARGE"));
+
+        Size[] enumArray = Size.values();
+        System.out.println(Arrays.toString(enumArray));
+
     }
 
 }
